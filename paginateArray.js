184a19619e2831a4c01fade5cb5ebebcb47dd8b9
WiktorKS <wiktorks1994@gmail.com>
2020-05-01 17:04:58 +0200

    Naming correction

--- paginateArray.js
+++ paginateArray.js
@@ -1,19 +1,24 @@
 // Stwórz funkcję paginateArray
+const isObjectWithCorrectKeys = object => {
+    return (typeof object == "object" && object !== null) && ("actualPageIdx" in object) && ("entriesOnPage" in object);
+}
+
+const isPositiveInteger = number => {
+    return (Number.isInteger(number) && number >= 0)
+}
+
 const paginateArray = (dataEntries, settings) => {
     if (!Array.isArray(dataEntries)) {
         throw new TypeError("First argument must be of type array");
     }
 
-    if (!(typeof settings == "object" && settings !== null) ||
-        !('actualPageIdx' in settings) || !('entriesOnPage' in settings)) {
-
+    if (!isObjectWithCorrectKeys(settings)) {
         throw new TypeError("Second argument must be of type object containing 'actualPageIdx' and 'entriesOnPage' keys");
     }
 
     const { actualPageIdx, entriesOnPage } = settings;
 
-    if ((!Number.isInteger(actualPageIdx) && actualPageIdx < 0) ||
-        (!Number.isInteger(entriesOnPage) && entriesOnPage < 0)) {
+    if (!isPositiveInteger(actualPageIdx) || !isPositiveInteger(entriesOnPage)) {
         throw new TypeError("Values in second argument's keys must be 0 or positive Integer")
     }
 
